281c5bcdc56b19839244f6703ff8cf292d7735cb
Bouafif Rayen <rayen.bouafif@insat.ucar.tn>
2025-01-28 18:57:50 +0100

    Created USART Header and source Files

---
+++ Drivers/Inc/stm32f401xx_usart.h
@@ -0,0 +1,35 @@
+/*
+ *
+  ******************************************************************************
+  * @file    stm32f401xx_usart.h
+  * @author  Rayen	Bouafif
+  * @mail    bouafifrayen01@gmail.com
+  * @Tel     (+216)29 049 373
+  * @date    28-01-2025
+  *****************************************************************************
+*
+*/
+
+#ifndef INC_STM32F401XX_USART_H_
+#define INC_STM32F401XX_USART_H_
+#include "stm32f401xx.h"
+
+
+
+/*
+ * Handler Structure for SPIx Peripheral
+ */
+
+typedef struct{
+
+}USARTx_Handler_t;
+
+
+
+/**************************************************************************************************
+ * 									APIs Supported by this driver
+ *					For further informations, please check the functions definition
+ **************************************************************************************************/
+
+
+#endif /* INC_STM32F401XX_USART_H_ */
---
+++ Src/stm32f401xx_usart.c
@@ -0,0 +1,17 @@
+/*
+ *
+  ******************************************************************************
+  * @file    stm32f401xx_usart.c
+  * @author  Rayen	Bouafif
+  * @mail    bouafifrayen01@gmail.com
+  * @Tel     (+216)29 049 373
+  * @date    28-01-2025
+  *****************************************************************************
+* This File Also contain the APIs explanation in the comments
+*/
+
+
+
+#include "stm32f401xx_gpio.h"
+
+

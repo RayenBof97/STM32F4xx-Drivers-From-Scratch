c2a0e708b4dfd9439bd8601bf07f1f3238ca6422
Bouafif Rayen <rayen.bouafif@insat.ucar.tn>
2025-02-01 18:10:04 +0100

    Fixed a bug

---
+++ Src/ArduinoTx.c
@@ -0,0 +1,80 @@
+/*
+ * ArduinoTx.c
+ *
+ *  Created on: Feb 1, 2025
+ *      Author: Rayen
+ */
+
+#include "stm32f401xx.h"
+#include "string.h"
+
+void SPI2_GPIOInit(void){
+
+	GPIOx_Handler_t spi_gpio;
+	memset(&spi_gpio,0,sizeof(spi_gpio));
+
+	spi_gpio.pGPIOx = GPIOC;
+	spi_gpio.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_ALTERNATE;
+	spi_gpio.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_3;
+	spi_gpio.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	spi_gpio.GPIO_PinConfig.GPIO_PinPuPdControl= GPIO_NO_PUPD;
+	spi_gpio.GPIO_PinConfig.PinOPType =  GPIO_OUT_TYPE_PP;
+	spi_gpio.GPIO_PinConfig.GPIO_pinAltFuncMode = GPIO_AF_5;
+	//Init MOSI
+	RB_GPIO_Init(&spi_gpio);
+
+	//Init SCLK
+	spi_gpio.pGPIOx = GPIOB;
+	spi_gpio.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_10;
+
+	RB_GPIO_Init(&spi_gpio);
+/*   REMOVE THE COMMENT IF YOU ARE USING THESE PINS
+	//Init MISO
+	spi_gpio.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_14;
+
+	RB_GPIO_Init(spi_gpio);
+
+	//Init NSS
+	spi_gpio.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_9;
+
+	RB_GPIO_Init(spi_gpio);
+*/
+}
+
+void SPI2_Init(void){
+	SPIx_Handler_t	spi2;
+	spi2.pSPIx = SPI2;
+	spi2.SPI_Config.SPI_BusConfig = SPI_BUS_CONFIG_FD;
+	spi2.SPI_Config.SPI_DeviceMode = SPI_MODE_MASTER;
+	spi2.SPI_Config.SPI_SclkSpeed = SPI_SCLK_SPEED_DIV2;
+	spi2.SPI_Config.SPI_DFF = SPI_DFF_8BITS;
+    spi2.SPI_Config.SPI_CPOL = SPI_CPOL_LOW;
+    spi2.SPI_Config.SPI_CPHA = SPI_CPHA_LOW;
+    spi2.SPI_Config.SPI_SSM = SPI_SM_SOFTWARE;
+
+    RB_SPI_Init(&spi2);
+
+}
+
+int main(){
+	char user_data[] = "Hello world";
+
+	//Initialisation
+	SPI2_GPIOInit();
+	SPI2_Init();
+	//Enable to avoid MODF Error (Pull NSS internally to high)
+	SPI_SSI_Config(SPI2,ENABLE);
+	SPI_PeriphControl(SPI2,ENABLE);
+	//Send Data
+	RB_SPI_Data_TX(SPI2,(uint8_t*)user_data,strlen(user_data));
+
+	//Disbale the peripheral
+	SPI_PeriphControl(SPI2,DISABLE);
+
+	while(1);
+
+
+return 0;
+}
+
+
